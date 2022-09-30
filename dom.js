9405560b91d938cc3bdcdb3120d82d9614bde355
shushpan-v-satori <ianastasieva@gmail.com>
2022-09-30 14:38:32 -0400

    Updated UI to work from Morse to English

--- dom.js
+++ dom.js
@@ -1,18 +1,38 @@
+import{translateToMorse} from "./translator.js";
 import{translateToEnglish} from "./translator.js";
+
+const toggleButton = document.querySelector(".languages__toggle")
+console.log(toggleButton);
 
 const translateEnglishButton = document.querySelectorAll(".input__translate")[0];
 console.log(translateEnglishButton);
 
 const inputStringDom = document.querySelectorAll(".input__string")[0];
 console.log(inputStringDom);
-console.log(inputStringDom.disabled);
+console.log(inputStringDom);
 
 const outputDom = document.querySelector(".output");
 console.log(outputDom);
 
+const inputDom = document.querySelector(".input")
+
 let inputString = '';
 let outputString='';
 let isClearOn = false;
+let isToMorse = true;
+
+toggleButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (isToMorse) {
+        toggleButton.innerHTML="from";
+        isToMorse = false;
+    }
+    else {
+        toggleButton.innerHTML="to";
+        isToMorse = true;
+    }
+    
+});
 
 translateEnglishButton.addEventListener("click", (event) => {
     event.preventDefault();
@@ -22,6 +42,7 @@ translateEnglishButton.addEventListener("click", (event) => {
         outputDom.innerHTML="";
         isClearOn = false;
         inputStringDom.disabled = false;
+        outputDom.style.borderBottom="none";
     }
     else{
         if (!inputStringDom.value) {
@@ -32,16 +53,20 @@ translateEnglishButton.addEventListener("click", (event) => {
             inputString=inputStringDom.value;
             console.log(inputString);
             outputDom.innerHTML=(`
-            <h1>Morse</h1>
             <div class="output__string">
             </div>`);
             const outputStringDom = document.querySelectorAll(".output__string")[0];
-            console.log(outputStringDom);
-            outputString = translateToEnglish(inputString); //need to link the translate function
+            if (isToMorse) {
+            outputString = translateToMorse(inputString); //need to link the translate function
+            }
+            else {
+            outputString = translateToEnglish(inputString); 
+            }
             outputStringDom.innerHTML=outputString;
             translateEnglishButton.innerHTML="Clear";
             isClearOn = true;
             inputStringDom.disabled = true;
+            outputDom.style.borderBottom="1px solid #d9d9d9";
             }
         }
 });
